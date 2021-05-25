c56b2727d63a7a9d2f6fbdffb8566a7128984c11
Artem Bazhenov <artem.bazhenov.325@mail.com>
2021-05-25 12:55:23 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.1b6ea50562cf8abf248f73f3d229fa90.js"
+  "./precache-manifest.54112d1f97b9ba5d1a7de19019b75e66.js"
 );
 
 self.addEventListener('message', (event) => {
